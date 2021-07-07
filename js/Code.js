e82db1582981fa6963f9a28371fcadbc812262c4
Codingstoff <73205292+Codingstoff@users.noreply.github.com>
2021-07-07 17:40:29 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+    fill("white")
+    textSize(15)
+    text("N C N O I T F U", 700,130)
+    fill("lightblue")
+    text("Hint: Performs a specific task", 700,160)
+    
+    fill("white")
+    textSize(15)
+    text("A A T B S A E D", 400,340)
+    fill("lightblue")
+    text("Hint: Stores Data", 400,380)
+}
